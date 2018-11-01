f5e6149251dfd3c2425814ba4c4c129734a03003
Evert Pot <me@evertpot.com>
2018-11-01 15:47:12 -0400

    exporting itself as middleware

---
+++ changelog.md
@@ -0,0 +1,14 @@
+Changelog
+=========
+
+0.3.2 (2018-??-??)
+------------------
+
+* The entire application now also exposes itself as a middleware, so it
+  can be customized and integrated into other curveball apps.
+
+
+0.3.1 (2018-11-01)
+------------------
+
+* First public version
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,7 @@
-import bodyParser from '@curveball/bodyparser';
 import { Application } from '@curveball/core';
-import session from '@curveball/session';
-import halBrowser from 'hal-browser';
+
 import process from 'process';
-import login from './middleware/login';
-import problem from './middleware/problem';
-import routes from './routes';
+import mainMw from './main-mw';
 
 const app = new Application();
 
@@ -15,37 +11,7 @@ app.use( async (ctx, next) => {
   console.log('<= %s', ctx.response.status);
 });
 
-
-app.use( async (ctx, next) => {
-
-  try {
-    await next();
-  } catch (e) {
-    console.error(e);
-    throw e;
-  }
-
-});
-
-app.use(halBrowser({
-  title: 'Auth API',
-}));
-
-app.use(problem());
-
-app.use(session({
-  store: 'memory'
-}));
-
-app.use(login());
-
-app.use((ctx, next) => {
-  return bodyParser()(ctx, next);
-});
-
-for (const route of routes) {
-  app.use(route);
-}
+app.use(mainMw());
 
 app.listen(parseInt(process.env.PORT, 10));
 console.log('Listening on port', process.env.PORT);

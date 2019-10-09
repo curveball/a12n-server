565878f5847f221c11d39636b0c9fbe368b60a15
JuHang Sin <juhangsin@gmail.com>
2019-10-08 23:47:52 -0400

    lint

--- src/reset-password/service.ts
+++ src/reset-password/service.ts
@@ -2,10 +2,10 @@ import nodemailer from 'nodemailer';
 import { User } from '../user/types';
 
 
-export async function sendResetPasswordEmail (user: User) {
+export async function sendResetPasswordEmail(user: User) {
 
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
+    const transporter = nodemailer.createTransport({
         host: '127.0.0.1',
         port: 25,
         secure: false, // true for 465, false for other ports
@@ -14,12 +14,12 @@ export async function sendResetPasswordEmail (user: User) {
         }
     });
     // send mail with defined transport object
-    let info = await transporter.sendMail({
+    const info = await transporter.sendMail({
         from: '"John Doe" <johndoe@badgateway.net>', // sender address
         to: user.identity.substring(7), // list of receivers
         subject: 'Reset Password Request', // Subject line
         text: 'Please click below link to reset your password!', // plain text body
         html: '<b>Please click below link to reset your password!</b>' // html body
     });
-    nodemailer.getTestMessageUrl(info)
+    nodemailer.getTestMessageUrl(info);
 }

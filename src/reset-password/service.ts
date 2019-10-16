46f17062ab7ddbbbc85a8fe7ccb951d6b14a76c2
JuHang Sin <juhangsin@gmail.com>
2019-10-15 21:40:59 -0400

    Making progress on Reset Password

--- src/reset-password/service.ts
+++ src/reset-password/service.ts
@@ -5,21 +5,16 @@ import { User } from '../user/types';
 export async function sendResetPasswordEmail(user: User) {
 
     // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-        host: '127.0.0.1',
-        port: 25,
-        secure: false, // true for 465, false for other ports
-        tls: {
-            rejectUnauthorized: false
-        }
-    });
+    const transporter = nodemailer.createTransport(process.env.SMTP_URL);
+
     // send mail with defined transport object
     const info = await transporter.sendMail({
-        from: '"John Doe" <johndoe@badgateway.net>', // sender address
+        from: '"John Doe" <system@pegasi.badgateway.net>', // sender address
         to: user.identity.substring(7), // list of receivers
         subject: 'Reset Password Request', // Subject line
         text: 'Please click below link to reset your password!', // plain text body
         html: '<b>Please click below link to reset your password!</b>' // html body
     });
+    
     nodemailer.getTestMessageUrl(info);
 }

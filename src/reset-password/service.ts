2522aa5642046f656b98b3e6f2b74f39bd8f873d
JuHang Sin <juhangsin@gmail.com>
2019-10-18 16:43:21 -0400

    Thorwing error message for environment varible

--- src/reset-password/service.ts
+++ src/reset-password/service.ts
@@ -1,20 +1,27 @@
 import nodemailer from 'nodemailer';
 import { User } from '../user/types';
 
-
 export async function sendResetPasswordEmail(user: User) {
 
-    // create reusable transporter object using the default SMTP transport
+    if (!process.env.SMTP_EMAIL_FROM) {
+        throw new Error('The environment variable SMTP_EMAIL_FROM must be set');
+    }
+
+    if (!process.env.SMPT_URL) {
+        throw new Error('The environment variable SMTP_URL must be set. Needs to contain "smtps://[Username]:[Password]@[Host]:[Port]"');
+    }
+
     const transporter = nodemailer.createTransport(process.env.SMTP_URL);
+
 
     // send mail with defined transport object
     const info = await transporter.sendMail({
-        from: '"John Doe" <system@pegasi.badgateway.net>', // sender address
+        from: '"Admin" <' + process.env.SMTP_EMAIL_FROM + '>', // sender address
         to: user.identity.substring(7), // list of receivers
-        subject: 'Reset Password Request', // Subject line
-        text: 'Please click below link to reset your password!', // plain text body
-        html: '<b>Please click below link to reset your password!</b>' // html body
+        subject: 'Reset Password Link', // Subject line
+        text: 'Please click link below to reset your password.', // plain text body
+        html: '<b>Please click link below to reset your password!</b>' // html body
     });
-    
+
     nodemailer.getTestMessageUrl(info);
 }

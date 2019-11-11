9fe40e18b1870b9a09d4a1a4500daf6ed79a9e75
JuHang Sin <juhangsin@gmail.com>
2019-11-11 12:52:48 -0500

    Added error if session storage is empty

--- src/reset-password/controller/token.ts
+++ src/reset-password/controller/token.ts
@@ -1,5 +1,6 @@
 import Controller from '@curveball/controller';
 import { Context } from '@curveball/core';
+import { Forbidden } from '@curveball/http-errors';
 import { resetPasswordForm } from '../formats/redirect';
 import { validateToken } from '../service';
 
@@ -9,6 +10,11 @@ class ResetPasswordTokenController extends Controller {
     const urlToken = ctx.state.params.token;
     const user = await validateToken(urlToken);
     ctx.state.session.resetPasswordUser = user;
+
+    if (!ctx.state.session.resetPasswordUser) {
+      throw new Forbidden('You can only use this endpoint after you went through the \'forgot password\' flow');
+    }
+
     ctx.response.type = 'text/html';
     ctx.response.body = resetPasswordForm(ctx.query.msg);
   }

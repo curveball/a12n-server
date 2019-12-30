faa6cd6fe57eab1c158ed85d2af5fa3c2d63e5a1
JuHang Sin <juhangsin@gmail.com>
2019-12-30 17:31:14 -0500

    Create a new 'privilege' collection

--- src/privilege/controller/collection.ts
+++ src/privilege/controller/collection.ts
@@ -1,6 +1,14 @@
 import Controller from '@curveball/controller';
+import { Context } from '@curveball/core';
+import * as privilegeService from '../service';
+import * as hal from '../formats/hal';
 
 class PrivilegeCollectionController extends Controller {
+
+  async get(ctx: Context) {
+    const privileges = await privilegeService.findPrivileges();
+    ctx.response.body = hal.collection(privileges)
+  }
 
 }
 

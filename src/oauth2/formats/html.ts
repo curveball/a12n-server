0214d4922775e526a5290da4e2786d04bf69a1b4
JuHang Sin <juhangsin@gmail.com>
2019-12-30 10:30:49 -0500

    adding TOTP options

--- src/oauth2/formats/html.ts
+++ src/oauth2/formats/html.ts
@@ -1,7 +1,25 @@
 import { render } from '../../templates';
 type KeyValue = { [key: string]: string };
 
-export function loginForm(msg: string, error: string, hiddenFields: KeyValue, registrationEnabled: boolean) {
+export function loginForm(msg: string, error: string, hiddenFields: KeyValue, registrationEnabled: boolean, totp: 'required' | 'optional' | 'disabled') {
+
+  let totpRequired;
+  let totpEnabled;
+
+  switch (totp) {
+    case 'required':
+      totpRequired = true;
+      totpEnabled = true;
+      break;
+    case 'optional':
+      totpRequired = false;
+      totpEnabled = true;
+      break;
+    case 'disabled':
+      totpRequired = false;
+      totpEnabled = false;
+      break;
+  }
 
   return render('login', {
     title: 'Login',
@@ -10,6 +28,8 @@ export function loginForm(msg: string, error: string, hiddenFields: KeyValue, re
     hiddenFields: hiddenFields,
     registrationEnabled,
     action: '/authorize',
+    totpRequired: totpRequired,
+    totpEnabled: totpEnabled
   });
 
 }

647cff804dcab6608886e53151d12a86e3ed59a1
Michael <Michael@MichaelHumiston.com>
2020-07-14 19:39:39 -0400

    Remove comment

--- src/mfa/webauthn/controller/register.ts
+++ src/mfa/webauthn/controller/register.ts
@@ -14,11 +14,6 @@ class WebAuthnRegistrationRequestController extends Controller {
   async get(ctx: Context) {
     const user: User = ctx.state.session.registerUser;
 
-    /**
-         * A new, random value needs to be generated every time an attestation is performed!
-         * The server needs to temporarily remember this value for verification, so don't lose it until
-         * after you verify an authenticator response.
-         */
     const challenge = crypto.randomBytes(64).toString('hex');
     ctx.state.session.webAuthnChallenge = challenge;
 
@@ -32,9 +27,9 @@ class WebAuthnRegistrationRequestController extends Controller {
       attestationType: 'direct',
       excludedCredentialIDs: (await webAuthnService.findDevicesByUser(user)).map(device => device.credentialID),
       /**
-             * The optional authenticatorSelection property allows for specifying more constraints around
-             * the types of authenticators that users to can use for attestation
-             */
+       * The optional authenticatorSelection property allows for specifying more constraints around
+       * the types of authenticators that users to can use for attestation
+       */
       authenticatorSelection: {
         authenticatorAttachment: 'cross-platform',
         userVerification: 'preferred',

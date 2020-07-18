a71f5fbf7263e6818c720502dffed45b67335ef8
Michael <Michael@MichaelHumiston.com>
2020-07-18 12:30:13 -0400

    Fix insert statement

--- mysql-schema/037-totp-registration.sql
+++ mysql-schema/037-totp-registration.sql
@@ -3,7 +3,7 @@ START TRANSACTION;
 
 INSERT INTO changelog VALUES (37, UNIX_TIMESTAMP());
 
-INSERT INTO server_settings (setting, value) VALUES ('totp.serviceName', '"Authentication API');
+INSERT INTO server_settings (setting, value) VALUES ('totp.serviceName', '"Authentication API"');
 
 ALTER TABLE user_totp
   ADD created INT UNSIGNED NOT NULL DEFAULT '0';

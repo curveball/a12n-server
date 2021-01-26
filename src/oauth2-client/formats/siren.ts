b858560a7bbbdf35f59b940065b12a89877faa24
Evert Pot <me@evertpot.com>
2021-01-26 00:26:35 -0500

    Some updates to OAuth2 'new client' form.
    Hopefully this will reduce friction a bit.

--- src/oauth2-client/formats/siren.ts
+++ src/oauth2-client/formats/siren.ts
@@ -24,12 +24,30 @@ export function newClient(user: User) {
             title: 'Client ID, leave blank to auto-generate',
           },
           {
-            name: 'allowedGrantTypes',
-            title: 'Allowed Grant Types (space separated)',
+            name: 'allowAuthorizationCode',
+            title: 'Allow "authorization_code" grant_type (for browser apps) ',
+            type: 'checkbox',
+          },
+          {
+            name: 'allowClientCredentials',
+            title: 'Allow "client_credentials" grant_type (for server to server apps) ',
+            type: 'checkbox',
+          },
+          {
+            name: 'allowImplicit',
+            title: 'Allow "implicit" grant_type (deprecated) ',
+            type: 'checkbox',
+          },
+          {
+            name: 'allowRefreshToken',
+            title: 'Allow "refresh_token" grant_type',
+            type: 'checkbox',
+            value: true,
           },
           {
             name: 'redirectUris',
             title: 'Redirect Uris (space separated)',
+            type: 'textarea',
           },
         ],
       }

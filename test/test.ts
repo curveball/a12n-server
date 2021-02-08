8627ecfc2f3c67f824d3762dfa54eec8cc28f44e
JuHang Sin <juhangsin@gmail.com>
2021-02-08 10:35:23 -0500

    lint

--- test/test.ts
+++ test/test.ts
@@ -3,24 +3,24 @@ import { expect } from 'chai';
 
 describe('URI Test', () => {
 
-  const expectedValue = '/user/1'
+  const expectedValue = '/user/1';
 
-    it('should pass with full url passed in', () => {
-      const url = 'http://test.com/user/1';
-      const result = getRelHref(url);
-      expect(result).to.equal(expectedValue);
-    });
+  it('should pass with full url passed in', () => {
+    const url = 'http://test.com/user/1';
+    const result = getRelHref(url);
+    expect(result).to.equal(expectedValue);
+  });
 
-    it('should pass with relative url being passed in', () => {
-      const url = '/user/1';
-      const result = getRelHref(url);
-      expect(result).to.equal(expectedValue);
-    });
+  it('should pass with relative url being passed in', () => {
+    const url = '/user/1';
+    const result = getRelHref(url);
+    expect(result).to.equal(expectedValue);
+  });
 
-    it('should fail if URL doesn`t contain the opening /', () => {
-      const url = 'user/1';
-      const result = getRelHref(url);
-      expect(result).to.not.equal(expectedValue);
-    });
+  it('should fail if URL doesn`t contain the opening /', () => {
+    const url = 'user/1';
+    const result = getRelHref(url);
+    expect(result).to.not.equal(expectedValue);
+  });
 
 });

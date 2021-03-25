7ac805321520380fd97f258de3dacf7369be1e37
JuHang Sin <juhangsin@gmail.com>
2021-03-25 10:57:32 -0400

    clean up

--- src/user/controller/password.ts
+++ src/user/controller/password.ts
@@ -2,7 +2,6 @@ import Controller from '@curveball/controller';
 import { Context } from '@curveball/core';
 import * as UserService from '../service';
 import { Forbidden } from '@curveball/http-errors';
-import { User } from '../types';
 import * as privilegeService from '../../privilege/service';
 
 class UserPasswordController extends Controller {
@@ -14,7 +13,12 @@ class UserPasswordController extends Controller {
     }
 
     const userBody: any = ctx.request.body;
-    const user = await UserService.findById(parseInt(ctx.params.id, 10)) as User;
+    const user = await UserService.findById(parseInt(ctx.params.id, 10))
+
+    if (user.type !== 'user') {
+      throw new Forbidden('You can only update passwords for users');
+    };
+
     const password = userBody.newPassword;
 
     await UserService.updatePassword(user, password);

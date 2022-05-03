cb5b680ab76b50db0c9e06021ea7e67ba9defcd2
Evert Pot <me@evertpot.com>
2022-05-03 02:41:21 -0400

    Make require_pkce non-nullable

--- src/migrations/20220503012552_fix-type-constraint.ts
+++ src/migrations/20220503012552_fix-type-constraint.ts
@@ -2,12 +2,18 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('oauth2_clients', table => {
-
-    table.boolean('require_pkce');
-
+    table
+      .boolean('require_pkce');
   });
   await knex('oauth2_client')
     .update({require_pkce: 0});
+
+  await knex.schema.alterTable('oauth2_clients', table => {
+    table
+      .boolean('require_pkce')
+      .notNullable()
+      .alter();
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {

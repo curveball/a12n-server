a02bbb8fe09c83c8c91dd543f8cf724f5cbb55a9
Evert Pot <me@evertpot.com>
2024-07-10 01:57:32 -0400

    Another __dirname instance

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,12 +1,14 @@
+import { dirname } from 'node:path';
 import { getSettings } from './database.js';
 import * as dotenv from 'dotenv';
+import { fileURLToPath } from 'node:url';
 
 if (process.env.PUBLIC_URI === undefined) {
   // If there's no PUBLIC_URI environment variable, it's a good indication
   // that we may be missing a .env file.
   //
   // This is the only required environment variable.
-  dotenv.config({path: __dirname + '/../.env'});
+  dotenv.config({path: dirname(fileURLToPath(import.meta.url)) + '/../.env'});
 }
 
 const settings = getSettings();

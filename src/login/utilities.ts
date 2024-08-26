9871a38a7df53188053e41e6b87a04f4f813b11d
Evert Pot <me@evertpot.com>
2024-08-26 12:47:51 -0400

    Allow for more valid continue urls.

--- src/login/utilities.ts
+++ src/login/utilities.ts
@@ -1,4 +1,3 @@
 export function isValidRedirect(url: string): boolean {
-  // For now, /authorize is the only valid URL to redirect to
-  return url.startsWith('/authorize');
+  return url.startsWith('/');
 }

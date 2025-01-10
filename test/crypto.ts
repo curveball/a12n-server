2ca61dfb94ac9832c5d7b282d1d09723954ff588
Evert Pot <me@evertpot.com>
2025-01-09 22:58:52 -0500

    Add a few unittests

--- test/crypto.ts
+++ test/crypto.ts
@@ -1,4 +1,4 @@
-import { generateSecretToken, uuidUrn } from '../src/crypto.js';
+import { generateSecretToken, uuidUrn, generatePublicId, generatePassword, generateVerificationDigits } from '../src/crypto.js';
 import { strict as assert } from 'node:assert';
 import { describe, it } from 'node:test';
 
@@ -9,7 +9,6 @@ describe('Crypto utilities', () => {
     it('should generate a 32-byte secret token', async () => {
 
       const token = await generateSecretToken();
-
       assert.equal(token.length,Math.ceil(32 / 3 * 4));
 
     });
@@ -32,6 +31,46 @@ describe('Crypto utilities', () => {
 
     });
 
+
+  });
+
+  describe('generatePublicId()', () => {
+
+    it('should generate a public id', async () => {
+
+      const id = await generatePublicId();
+      assert.equal(id.length, 11);
+
+    });
+
+  });
+
+  describe('generatePassword()', () => {
+
+    it('should generate a password', () => {
+
+      const password = generatePassword();
+      assert.match(password, /^[a-z-]{20,}$/);
+
+    });
+
+  });
+
+  describe('generateVerificationDigits()', () => {
+
+    it('should generate a verification code', () => {
+
+      const code = generateVerificationDigits();
+      assert.match(code, /^[0-9]{6}$/);
+
+    });
+
+    it('should generate a verification code of the requested length', () => {
+
+      const code = generateVerificationDigits(8);
+      assert.match(code, /^[0-9]{8}$/);
+
+    });
 
   });
 

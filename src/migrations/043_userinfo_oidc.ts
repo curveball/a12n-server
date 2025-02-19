219bc268f2eda4c9e6525adbf0528fb07ba87963
Jen Chan <jennifer@rivalry.com>
2025-02-18 23:33:49 -0500

    chore: adds address claims, adds regex for checking locale

--- src/migrations/043_userinfo_oidc.ts
+++ src/migrations/043_userinfo_oidc.ts
@@ -16,25 +16,36 @@ export async function up(knex: Knex): Promise<void> {
 
     await knex.schema.createTable('user_info', table => {
         table.increments()
+        table.foreign('user_id').references('id').inTable('principals');
         table.string('sub', 50).notNullable()
         table.string('name', 200).nullable()
         table.string('email', 100).nullable()
+        table.boolean('email_verified').nullable()
+        table.string('middle_name').nullable()
         table.string('given_name', 30).nullable()
         table.string('family_name', 30).nullable()
+        // TODO: Check validation
+        table.date('birthdate').nullable()
         table.string('nickname').nullable()
         table.string('preferred_username', 100).nullable()
         table.string('profile', 2048).nullable()
         table.string('picture').nullable().checkRegex('^(http://|https://).+\\.(png|jpeg|jpg|gif).*$')
         table.integer('updated_at').unsigned().nullable()
-        table.boolean('email_verified').nullable()
         table.string('phone_number', 200).nullable()
         table.boolean('phone_number_verified').nullable()
         table.string('website', 300).nullable().checkRegex('^(http://|https://).*$')
-        table.json('address').nullable()
+        // TODO: Check for right way to add JSON properties
+        table.json('address').defaultTo({
+            "formatted": '',
+            "street_address": '',
+            "locality": '',
+            "region": '',
+            "postal_code": '',
+            "country": ''
+        })
         table.enu('gender', ['male', 'female', 'other']).nullable()
-        table.string('locale', 5).nullable()
+        table.string('locale', 5).nullable().checkRegex('^[a-z]{2}-[A-Z]{2}$')
         table.string('zoneinfo').nullable()
-        table.foreign('user_id').references('id').inTable('principals');
       });
     
 }

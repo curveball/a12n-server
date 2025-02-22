8513f9022cc07f432f123433d66fa2ae91825e42
jen chan <6406037+usrrname@users.noreply.github.com>
2025-02-22 00:21:27 -0500

    #624 feat: `user_info` table (#631)
    creates user_info table schema
    Source: https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
    
    Co-authored-by: Evert Pot <me@evertpot.com>

---
+++ src/migrations/20251740066033_user_info.ts
@@ -0,0 +1,26 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+
+  await knex.schema.createTable('user_info', table => {
+    table.integer('principal_id').unsigned().notNullable().primary().references('principals.id');
+    table.string('name', 200).nullable().comment('End-User\'s full name in displayable form including all name parts, possibly including titles and suffixes, ordered according to the End-User\'s locale and preferences.');
+    table.string('middle_name').nullable().comment('Middle name(s) of the End-User.');
+    table.string('given_name', 30).nullable().comment('Given name(s) or first name(s) of the End-User.');
+    table.string('family_name', 30).nullable().comment('Surname(s) or last name(s) of the End-User.');
+    table.date('birthdate').nullable().comment('End-User\'s birthday');
+    table.text('address').nullable().comment('End-User\'s preferred postal address. ');
+    table.string('locale', 5).nullable().comment('End-User\'s locale. For example, en-US or fr-CA.');
+    table.string('zoneinfo').nullable().comment('End-User\'s time zone. For example, Europe/Paris or America/Los_Angeles.');
+    table.integer('created_at').unsigned().comment('Time the user_info record was created.');
+    table.integer('modified_at').unsigned().nullable().comment('Time the End-User\'s information was last updated.');
+    table.text('metadata');
+  });
+
+}
+
+export async function down(knex: Knex): Promise<void> {
+
+  throw new Error('This migration doesn\'t have a "down" script');
+
+}

26d4c7ed90c367e129df2bfc26ede67da1fb95f2
jen chan <6406037+usrrname@users.noreply.github.com>
2025-08-07 17:08:10 -0400

    fix: export contract types (`api-types.ts`)  (#687)
    * fix: export contract types and add comments to clarifying internal types for project
    
    * Update src/index.ts
    
    Co-authored-by: Evert Pot <me@evertpot.com>
    
    ---------
    
    Co-authored-by: Evert Pot <me@evertpot.com>

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@
  * exposes a middleware for curveball, and a load function that
  * needs to be called to initialize the server
  */
+
 export { default as mainMw } from './main-mw.ts';
 export { load as init } from './server-settings.ts';
 
+// db-types.ts registers its types globally and is not explicitly
+// imported by other files. This ensures that Typescript doesn't miss these.
 import './db-types.ts';
+
+/** contract types */
+export * from './api-types.ts';

051f17c9b85331c3e996a279eb37939d7ce756a5
Jen Chan <6406037+usrrname@users.noreply.github.com>
2025-08-07 16:22:59 -0400

    fix: export contract types and add comments to clarifying internal types for project

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,12 @@
  * exposes a middleware for curveball, and a load function that
  * needs to be called to initialize the server
  */
+
 export { default as mainMw } from './main-mw.ts';
 export { load as init } from './server-settings.ts';
 
+/** db-types and src/types are internal to this project */
 import './db-types.ts';
+
+/** contract types */
+export * from './api-types.ts';
